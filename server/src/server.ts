cf0916d926451b05a82d4dc5297d1c3251cdb51d
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-20 16:00:41 +0600

    Configure express server

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import cors from "cors"; // library for cross-origin resource sharing
+import express from "express";
+import morgan from "morgan"; // library for logging http requests
+
+const app = express();
+
+app.use(cors()); // allow all origins
+app.use(morgan("dev")); // log status codes
+app.use(express.json()); // parse json bodies (content-type: application/json)
+app.use(express.urlencoded({ extended: true })); // parse urlencoded bodies (content-type: application/x-www-form-urlencoded)
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "hi from server" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({ message: `had an error: ${err.message}` });
+});
+
+export default app;
