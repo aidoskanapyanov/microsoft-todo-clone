ac36a7e60cbfbe2ab1a822856d2d7f4018256db7
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-06-20 17:00:36 +0600

    Add user model

---
+++ server/prisma/migrations/20230620104754_user/migration.sql
@@ -0,0 +1,19 @@
+-- AlterTable
+ALTER TABLE "Todo" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
