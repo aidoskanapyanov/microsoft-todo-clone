4f3f7f227df3ee589e3624155a41ef8092ed6c11
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-02 22:00:04 +0600

    Revert back to base frontend code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import Sidebar from "@/components/sidebar";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="p-6">
-        <Sidebar />
-      </div>
+      <div>Hello, World!</div>
     </BrowserRouter>
   );
 }
