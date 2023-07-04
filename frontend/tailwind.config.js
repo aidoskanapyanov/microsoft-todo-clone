f5983b5667bdc5e6ebe2ba318756cdc8b4c4cdd7
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-07-04 18:16:27 +0600

    Revert "Overwrite tw height for mobile"
    This reverts commit b40dae570d65aa365e1d4cbcccccf3e797133374.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,18 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    height: (theme) => ({
-      auto: "auto",
-      ...theme("spacing"),
-      full: "100%",
-      screen: "calc(var(--vh) * 100)",
-    }),
-    minHeight: (theme) => ({
-      0: "0",
-      ...theme("spacing"),
-      full: "100%",
-      screen: "calc(var(--vh) * 100)",
-    }),
     extend: {
       colors: {
         background: "hsl(var(--background))",
